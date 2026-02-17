80afb0dd74a92b58d736776abe1947cd0e1745a7
Rohit Raj <rajrohit7388@outlook.com>
2026-02-17 15:49:12 +0530

    Add files via upload

---
+++ pattern7.java
@@ -0,0 +1,26 @@
+import java.util.*;
+class pattern7
+{
+    public static void main(String []args)
+    {
+        Scanner sc = new Scanner(System.in);
+
+        // digit triangle
+
+        int number;
+        System.out.print("enter number n : ");
+        number = sc.nextInt();
+
+        int i;
+        int j;
+
+        for(i=1; i<=number; i++)
+        {
+            for(j=1; j<=i; j++)
+            {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}
