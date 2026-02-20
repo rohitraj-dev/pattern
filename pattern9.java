fe3d83a9eb9a3eee7e1a82e371c7fe4997eca153
Rohit Raj <rajrohit7388@outlook.com>
2026-02-20 15:57:08 +0530

    Add files via upload

---
+++ pattern9.java
@@ -0,0 +1,33 @@
+import java.util.*;
+class pattern9
+{
+    public static void main(String []args)
+    {
+        Scanner sc = new Scanner(System.in);
+
+        //number + alphabet triangle
+
+        int number;
+        System.out.print("enter number n : ");
+        number = sc.nextInt();
+
+        int i;
+        int j;
+
+        for(i=1; i<=number; i++)
+        {
+            for(j=1; j<=i; j++)
+            {
+                if(i%2 == 1)
+                {
+                    System.out.print(j);
+                }
+                else
+                {
+                    System.out.print((char)(j+64));
+                }
+            }
+            System.out.println();
+        }
+    }
+}
