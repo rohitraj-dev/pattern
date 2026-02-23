39a9503a8a09b690e9eee32766a0e847928a1651
Rohit Raj <rajrohit7388@outlook.com>
2026-02-23 15:33:12 +0530

    Add files via upload

---
+++ pattern10.java
@@ -0,0 +1,33 @@
+import java.util.*;
+class pattern10
+{
+    public static void main(String []args)
+    {
+        Scanner sc = new Scanner(System.in);
+
+        //number + alphabet triangle
+
+        int number;
+        System.out.print("enter number n : ");
+        number = sc.nextInt();
+
+        int i;
+        int j;
+
+        for(i=1; i<=number; i++)
+        {
+            for(j=1; j<=i; j++)
+            {
+                if(i%2 == 1)
+                {
+                    System.out.print(i);
+                }
+                else
+                {
+                    System.out.print((char)(j+64));
+                }
+            }
+            System.out.println();
+        }
+    }
+}
